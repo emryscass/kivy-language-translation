cf27593d1a535f9f9fd26cc9c8ee9a68e2331d3a
emrys <emryscass@gmail.com>
2020-02-16 11:39:32 -0500

    a new project working with the kivy framework to translate languages

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # kivy-language-translation
-Translate any language to English 
+Translate any language to English
+
+Currently translate any phrase from any language to English. Plan to add functionality to translate any language to any language.
+
+Features to be added:
+	* translate back to original language
+	* translate from any language to any language 
---
+++ language.py
@@ -0,0 +1,24 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from googletrans import Translator
+
+
+
+class Translate(GridLayout):
+
+    def translate(self, *args):
+        translator = Translator()
+        translated = translator.translate(*args)
+        self.lbl.text = translated.text
+
+
+
+
+class LanguageApp(App):
+
+    def build(self):
+        return Translate()
+
+
+if __name__ == '__main__':
+    LanguageApp().run()
